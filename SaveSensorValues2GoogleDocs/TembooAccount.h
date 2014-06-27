c86713db38c23560e6adbd500d36a6ea1383b361
FluuuxTechBlog <profwebapps@fluuux.de>
2014-06-27 05:51:40 +0200

    TembooAccount.h
    Diese Datei enthält die Zugangsdaten zum Google und zum Temboo Account

--- SaveSensorValues2GoogleDocs/TembooAccount.h
+++ SaveSensorValues2GoogleDocs/TembooAccount.h
@@ -1,9 +1,11 @@
+// Google Account
 const String GOOGLE_USERNAME = "EINFÜGEN";
 const String GOOGLE_PASSWORD = "EINFÜGEN";
 const String SPREADSHEET_TITLE = "Yun";
 const String TO_EMAIL_ADDRESS = "EINFÜGEN";
 
 
+// Temboo Account
 #define TEMBOO_ACCOUNT "EINFÜGEN"  // Your Temboo account name 
 #define TEMBOO_APP_KEY_NAME "myFirstApp"  // Your Temboo app key name
 #define TEMBOO_APP_KEY "EINFÜGEN"  // Your Temboo app key
