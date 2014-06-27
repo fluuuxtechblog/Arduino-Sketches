450d440a66e8a58c7ec589b90a8d6ba43154d725
FluuuxTechBlog <profwebapps@fluuux.de>
2014-06-27 13:44:01 +0200

    eMail bei Auslösen eines Sensors senden

---
+++ YunSendMail/GoogleAccount.h
@@ -0,0 +1,4 @@
+#define GOOGLE_USERNAME "profwebapps@gmail.com"
+#define GOOGLE_PASSWORD "sajfufojfmodqyar"
+#define SPREADSHEET_TITLE "Yun"
+#define TO_EMAIL_ADDRESS "profwebapps@fluuux.de"
---
+++ YunSendMail/TembooAccount.h
@@ -0,0 +1,3 @@
+#define TEMBOO_ACCOUNT "fluuuxtechblog"  // Your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // Your Temboo app key name
+#define TEMBOO_APP_KEY "yxn152Eigjefrhae40cnv8wsQUU4SCup"  // Your Temboo app key
