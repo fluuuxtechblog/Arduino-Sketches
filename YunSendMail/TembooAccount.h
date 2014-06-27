3e146d6940a7bbf1bf627da231a05b5934bb918c
FluuuxTechBlog <profwebapps@fluuux.de>
2014-06-27 13:46:03 +0200

    Update TembooAccount.h

--- YunSendMail/TembooAccount.h
+++ YunSendMail/TembooAccount.h
@@ -1,3 +1,3 @@
-#define TEMBOO_ACCOUNT "fluuuxtechblog"  // Your Temboo account name 
+#define TEMBOO_ACCOUNT ""  // Your Temboo account name 
 #define TEMBOO_APP_KEY_NAME "myFirstApp"  // Your Temboo app key name
-#define TEMBOO_APP_KEY "yxn152Eigjefrhae40cnv8wsQUU4SCup"  // Your Temboo app key
+#define TEMBOO_APP_KEY ""  // Your Temboo app key
